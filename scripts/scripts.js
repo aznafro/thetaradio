b9eb3810e9d35a139641916e81343ef978cdbf45
aznafro <joekwon123@yahoo.com>
2018-10-02 20:04:09 +0800

    added the gear section. included the swipe js library.  currently in the middle of the channels section.

---
+++ scripts/scripts.js
@@ -0,0 +1,30 @@
+document.querySelector(".menu-box").addEventListener("click", function() {
+	document.querySelector(".nav__links-box").classList.toggle("display-nav");
+	document.querySelector("html").classList.toggle("menu-toggle");
+	// document.querySelector("header").classList.toggle("blur");
+	document.querySelectorAll("body > *:not(nav)").forEach(function(section) {
+		section.classList.toggle("blur");
+	});
+
+});
+
+var navLinks = document.querySelectorAll(".nav__links li a");
+for(var i=0; i < navLinks.length; i++) {
+	var link = navLinks[i];
+	link.addEventListener("click", function() {
+		for(var j=0; j < navLinks.length; j++) {
+			navLinks[j].classList.remove("link-hover");
+		}
+		this.classList.add("link-hover");
+	});
+};
+
+var currentY = 0;
+window.addEventListener("scroll", function() {
+	if(window.scrollY > currentY && window.scrollY >= window.innerHeight) {
+		document.querySelector("nav").classList.add("nav-sticky");
+	} else if(window.scrollY < currentY && window.scrollY <= window.innerHeight) {
+		document.querySelector("nav").classList.remove("nav-sticky");
+	}
+	currentY = window.scrollY;
+})
